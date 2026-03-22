51b277c8ef23912d733c5b2aa15921d038d6ddf6
AngelGaitanG <angel.gaitan.ev@gmail.com>
2026-03-22 20:41:20 -0300

    feat: Add Cloudinary provider for NestJS configuration.

--- src/common/cloudinary/cloudinary.provider.ts
+++ src/common/cloudinary/cloudinary.provider.ts
@@ -5,7 +5,7 @@ export const CloudinaryProvider = {
   provide: 'CLOUDINARY',
   useFactory: (configService: ConfigService) => {
     return v2.config({
-      cloud_name: configService.get<string>('CLOUDINARY_NAME'),
+      cloud_name: configService.get<string>('CLOUDINARY_CLOUD_NAME'),
       api_key: configService.get<string>('CLOUDINARY_API_KEY'),
       api_secret: configService.get<string>('CLOUDINARY_API_SECRET'),
     });
